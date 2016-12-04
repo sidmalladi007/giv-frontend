7165a6edc33f98f6c23b5bb5aed84ddc18b5c201
Akash Kejriwal <akash@kejriwal.com>
2016-12-04 04:25:21 -0500

    init

---
+++ Giv/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  Giv
+//
+//  Created by Akash Kejriwal on 12/4/16.
+//  Copyright © 2016 Akash Kejriwal. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
