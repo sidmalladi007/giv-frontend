70dc3738fdd5ee9af1d8708f6770e77f9a5458c9
Akash Kejriwal <akash@kejriwal.com>
2016-12-06 06:28:05 -0500

    added viewcharity, requestcharity, working on segues

---
+++ Giv/AddCharityViewController.swift
@@ -0,0 +1,24 @@
+//
+//  AddCharityViewController.swift
+//  Giv
+//
+//  Created by Akash Kejriwal on 12/4/16.
+//  Copyright © 2016 Akash Kejriwal. All rights reserved.
+//
+
+import UIKit
+
+class AddCharityViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+}
---
+++ Giv/FourthViewController.swift
@@ -0,0 +1,24 @@
+//
+//  FourthViewController.swift
+//  Giv
+//
+//  Created by Akash Kejriwal on 12/4/16.
+//  Copyright © 2016 Akash Kejriwal. All rights reserved.
+//
+
+import UIKit
+
+class FourthViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+}
